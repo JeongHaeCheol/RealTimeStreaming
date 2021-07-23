8be2a68fc54b78dc79ec5e0a86ae98559a30bf87
JeongHaeCheol <eiop77@naver.com>
2021-07-23 13:25:49 +0900

    config.application 파일 추가 gitignore에 의해 git에는 등록되지 않음

--- src/main/java/com/sun/fe/RealTimeStreamingApplication.java
+++ src/main/java/com/sun/fe/RealTimeStreamingApplication.java
@@ -2,8 +2,10 @@ package com.sun.fe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("config/config.properties")
 public class RealTimeStreamingApplication {
 
 	public static void main(String[] args) {
